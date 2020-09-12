5a82d6fcce84ac8bf5d053e8f6c25680e16ce892
ama31337 <70890873+ama31337@users.noreply.github.com>
2020-09-12 09:49:12 +0700

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -3,6 +3,6 @@ echo "installing vim configs..."
 echo "after complete run :PluginInstall in vim"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
-git clone https://github.com/boo50/vim.git && 
+git clone https://github.com/ama31337/vim.git && 
 cd vim && mv vimrc ~/.vimrc && rm -rf ~/vim &&
 vim
